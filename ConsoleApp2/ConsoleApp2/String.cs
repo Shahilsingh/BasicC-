58171f21f71beabec02f2175cb79f8959f035704
Your Name <shahil.bizruntime@gmail.com>
2018-10-11 21:04:26 +0530

    commit

---
+++ ConsoleApp2/ConsoleApp2/String.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class String
+    {
+        static void Main(string[] args)
+        {
+            string s = "100";
+            Console.WriteLine(Convert.ToInt32(s));
+            Console.WriteLine(int.Parse(s));
+            Console.read();
+        }
+    }
+}
